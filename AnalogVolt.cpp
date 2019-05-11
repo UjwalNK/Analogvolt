eb25e3810a1f4759d1c284c7560efa3d8f349b5f
UjwalNK <44840915+UjwalNK@users.noreply.github.com>
2019-05-11 18:38:03 +0530

    Update AnalogVolt.cpp
    Added doc comments

--- AnalogVolt.cpp
+++ AnalogVolt.cpp
@@ -5,32 +5,32 @@
 */
 
 #include "Arduino.h"
-#include "AnalogVolt.h"
+#include "AnalogVoltage.h"
 
-double AnalogVolt::aWriteVolt(double volt){
-    return AnalogVolt::convert2Value(volt);
+double AnalogVoltage::aWriteVolt(double volt){
+    return AnalogVoltage::convert2Value(volt);
 }
 
-void AnalogVolt::aWriteVolt(int pin, double volt){
+void AnalogVoltage::aWriteVolt(int pin, double volt){
     pinMode(pin, OUTPUT);
-    analogWrite(pin, AnalogVolt::convert2Value(volt));
+    analogWrite(pin, AnalogVoltage::convert2Value(volt));
 }
 
-double AnalogVolt::aReadVolt(int pin, int precision){
+double AnalogVoltage::aReadVolt(int pin, int precision){
     pinMode(pin, INPUT);
-    double volt = AnalogVolt::convert2Volt(analogRead(pin));
+    double volt = AnalogVoltage::convert2Volt(analogRead(pin));
     return volt;
 }
 
 //Convertion functions:
 
-double AnalogVolt::convert2Value(double value){
+double AnalogVoltage::convert2Value(double value){
     double volt = 0;
     volt = map(value, 0, 5, 0, 255);
     return volt;
 }
 
-double AnalogVolt::convert2Volt(double volt){
+double AnalogVoltage::convert2Volt(double volt){
     double value = 0;
     volt = map(value, 0, 255, 0, 5);
     return value;
